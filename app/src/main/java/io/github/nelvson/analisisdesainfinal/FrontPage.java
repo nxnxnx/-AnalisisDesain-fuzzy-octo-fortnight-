a4e971d0701fce5b6e365910db1a19570f4dcb4e
nelvson <nelvson1337@gmail.com>
2016-12-05 03:28:17 +0700

    initialized html on frontpage

--- app/src/main/java/io/github/nelvson/analisisdesainfinal/FrontPage.java
+++ app/src/main/java/io/github/nelvson/analisisdesainfinal/FrontPage.java
@@ -2,14 +2,22 @@ package io.github.nelvson.analisisdesainfinal;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 /**
  * Created by nelvson on 04/12/16.
  */
 
 public class FrontPage extends Activity {
+    WebView webView;
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.frontpagexml);
+        webView  = (WebView)findViewById(R.id.webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setJavaScriptEnabled(true);
     }
 }
